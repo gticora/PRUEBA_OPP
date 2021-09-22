1240a93d09f201506091eb683391d21782055919
Gustavo Ticora <gaticora@gmail.com>
2021-09-21 20:59:11 -0500

    estilos a formularios iompelmentando boostrap

--- calculadora.php
+++ calculadora.php
@@ -7,16 +7,16 @@
 		
 	</style>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </head>
 <body>
-<form action="calculapost.php" method="post">
-	<div>
-		<span>Ingrese el primer valor</span>
-		<span><input type="text" name="valor1"></span>
-	</div>
-	<div>
-		<span>Operador</span>
-		<span><select name="operacion" id="operacion">
+<form class="row g-3" action="calculapost.php" method="post">
+	<div class="col-md-6">
+	<label for="inputEmail4" class="form-label"><b>Calculadora</b></label>
+		<input type="text" name="valor1" class="form-control" id="valor1" placeholder="Ingrese el primer valor" required>
+		<label for="inputAddress2" class="form-label"></label>
+		<select name="operacion" class="form-control" id="operacion">
 			<option value="0">seleccione</option>
 			<option value="1">Suma</option>
 			<option value="2">Resta</option>
@@ -24,15 +24,13 @@
 			<option value="4">Division</option>
 			<option value="5">Porcentaje</option>
 			<option value="6">Raiz cuadrada</option>
-		</select></span>
+		</select>
+		<label for="inputAddress2" class="form-label"></label>
+		<input type="text" style="display:none;" name="valor2" class="form-control valor2" id="valor2" placeholder="Ingrese el segundo valor" required>
+		<label for="inputAddress2" class="form-label"></label>
 	</div>
-
-	<div class="valor2" style="display: none;">
-		<span>Ingrese el segundo valor</span>
-		<span><input type="text" name="valor2"></span>
-	</div>
-	<div>
-		<span><input type="submit" name="Calculadora"></span>
+	<div class="col-12">
+		<button class="btn btn-primary" type="submit">Calculadora</button>
 	</div>
 </form>
 </body>
--- index.php
+++ index.php
@@ -15,36 +15,40 @@ $cantidad=$row["cantidad"];
 <html>
 <head>
 	<title>Prueba</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </head>
 <body>
-<form action="formpost.php" method="post">
-	<div>
-		<span>Nombres</span>
-		<span><input type="text" name="nombres"></span>
+<section>
+<div class="card mb-3">
+	<div class="card-body">
+		<form class="row g-3" action="formpost.php" method="post">
+			<div class="col-md-6">
+				<label for="inputEmail4" class="form-label"><b>Registro de programador</b></label>
+				<input type="text" name="nombres" class="form-control" id="nombres" placeholder="Nombres Completos" required>
+				<label for="inputAddress2" class="form-label"></label>
+				<input type="text" name="apellidos" class="form-control" id="apellidos" placeholder="Apellidos Completos" required>
+				<label for="inputAddress2" class="form-label"></label>
+				<input type="text" name="cedula" class="form-control" id="cedula" placeholder="Documento de identidad" required>
+				<label for="inputAddress2" class="form-label"></label>
+				<input type="email" name="correo" class="form-control" id="correo" placeholder="Correo electronico" required>
+				<label for="inputAddress2" class="form-label"></label>
+				<input type="text"  name="lenguajes" class="form-control" id="lenguajes" placeholder="lenguajes">
+				<label for="inputAddress2" class="form-label"></label>
+			</div>
+			<div class="col-12">
+			<button class="btn btn-primary" type="submit">Button</button>
+			</div>
+		</form>
 	</div>
-	<div>
-		<span>Apellidos</span>
-		<span><input type="text" name="apellidos"></span>
-	</div>
-	<div>
-		<span>Cedula</span>
-		<span><input type="number" name="cedula"></span>
-	</div>
-	<div>
-		<span>Email</span>
-		<span><input type="email" name="correo"></span>
-	</div>
-	<div>
-		<span>Lenguajes</span>
-		<span><input type="text" name="lenguajes"></span>
-	</div>
-	<div>
-		<span><input type="submit" name="Enviar"></span>
-	</div>
-</form>
-<br>
-<div>
-	<span>Cantidad de programadores  registrados son: <?=$cantidad?></span>
 </div>
+<div class="card-body">
+	<h5 class="card-title"><span></span></h5>
+	<p class="card-text"><span><b>Cantidad de programadores  registrados son: <?=$cantidad?></b></span></p>
+	<p class="card-text">
+		<small class="text-muted"></small>
+	</p>
+</div>
+</section>
 </body>
 </html>
